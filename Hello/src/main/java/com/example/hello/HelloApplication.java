8383d21ad6ec6e71c82a15b41b4258867ef6af66
Lebedeva Alena <114668875+Archer1765@users.noreply.github.com>
2025-05-18 16:06:40 +0500

    Create HelloApplication.java

---
+++ Hello/src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,27 @@
+package com.example.hello;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 540);
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("/logo.png")));
+        stage.setTitle("Быки и коровы");
+        stage.setResizable(false);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
